f9afe810a73bb186cc2b44624976a00bc268aa86
saket <skintali@hawk.iit.edu>
2016-03-25 16:45:59 -0400

    header features added for all screens

--- js/app.js
+++ js/app.js
@@ -1,94 +1,106 @@
 $(document).foundation();
 
-$(document).ready(function(){
-    $(".startorder").click(function(){
-        $("#initial-content").slideToggle(400);
-      $('.sign-in').hide();
-    });
-});
+// $(document).ready(function(){
+//     $(".startorder").click(function(){
+//         $("#initial-content").slideToggle(400);
+//       $('.sign-in').hide();
+//     });
+// });
 
-$(document).ready(function(){
-    $(".label-round").click(function(){
-        $("#initial-content").slideUp();
-    });
-});
+// $(document).ready(function(){
+//     $(".label-round").click(function(){
+//         $("#initial-content").slideUp();
+//     });
+// });
 
-$(document).ready(function(){
-    $(".create-account").click(function(){
-      $('#initial-content').hide();
-        $(".sign-in").slideToggle(400);
-    });
-});
+// $(document).ready(function(){
+//     $(".create-account").click(function(){
+//       $('#initial-content').hide();
+//         $(".sign-in").slideToggle(400);
+//     });
+// });
 
-$(document).ready(function(){
-    $(".label-rounds").click(function(){
-        $(".sign-in").slideUp();
-    });
-});
+// $(document).ready(function(){
+//     $(".label-rounds").click(function(){
+//         $(".sign-in").slideUp();
+//     });
+// });
 
-$(document).ready(function() {
- 
-  $("#owl-demo").owlCarousel({
- 
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 100,
-    autoPlay: 3000,
-      paginationSpeed : 400,
-      singleItem:true
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
- 
-  });
- 
-});
+// $(document).ready(function(){
+//     $(".label-round").click(function(){
+//         $("#initial-content").slideUp();
+//     });
+// });
 
-$(document).ready(function() {
+// $(document).ready(function() {
  
-  $("#owl-demo-1").owlCarousel({
+//   $("#owl-demo").owlCarousel({
  
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 100,
-    autoPlay: 3000,
-      paginationSpeed : 400,
-      singleItem:true
+//       navigation : false, // Show next and prev buttons
+//       slideSpeed : 100,
+//     autoPlay: 3000,
+//       paginationSpeed : 400,
+//       singleItem:true
  
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
+//       // "singleItem:true" is a shortcut for:
+//       // items : 1, 
+//       // itemsDesktop : false,
+//       // itemsDesktopSmall : false,
+//       // itemsTablet: false,
+//       // itemsMobile : false
  
-  });
+//   });
  
-});
+// });
 
-$(document).ready(function(){
-    $(".start-order").click(function(){
-      $(".main-menu").hide();
-        $(".store-info-hidden").show();
+// $(document).ready(function() {
+ 
+//   $("#owl-demo-1").owlCarousel({
+ 
+//       navigation : false, // Show next and prev buttons
+//       slideSpeed : 100,
+//     autoPlay: 3000,
+//       paginationSpeed : 400,
+//       singleItem:true
+ 
+//       // "singleItem:true" is a shortcut for:
+//       // items : 1, 
+//       // itemsDesktop : false,
+//       // itemsDesktopSmall : false,
+//       // itemsTablet: false,
+//       // itemsMobile : false
+ 
+//   });
+ 
+// });
+
+// $(document).ready(function(){
+//     $(".start-order").click(function(){
+//       $(".main-menu").hide();
+//         $(".store-info-hidden").show();
       
-    });
-});
+//     });
+// });
 
 
 
+
+// $(document).ready(function(){
+//     $(".carryout").click(function(){
+//         $(".hidden").slideToggle(400);
+      
+//     });
+// });
 
 $(document).ready(function(){
-    $(".carryout").click(function(){
-        $(".hidden").slideToggle(400);
-      
+    $(".sign-in").click(function(){
+        $(".sign-bar").slideToggle(500);
+         $(this).toggleClass("active");
     });
 });
-
 $(document).ready(function(){
-    $(".delivery").click(function(){
-        $(".del-hidden").slideToggle(500);
-      
+    $(".close-button").click(function(){
+        $(".sign-bar").slideUp();
+         $('.sign-in').removeClass("active");
     });
 });
